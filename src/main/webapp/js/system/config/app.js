ec910581b94578aa3090f34cf08b765d3e29ff2b
a021792876p <a021792876p@s09-pc19.ausias.lan>
2019-01-24 20:54:22 +0100

    puto desastre de mierda que no se por donde empezar

---
+++ src/main/webapp/js/system/config/app.js
@@ -0,0 +1,45 @@
+'use strict'
+
+var SuportWeb4All = angular.module('MyApp', [
+    'ngRoute',
+    'services',
+    'commonControllers',
+    'loginControllers',
+    'moduleComponent',
+    'ngMaterial',
+    'Directives'
+
+]).config(function ($mdDateLocaleProvider) {
+    // Example of a Spanish localization.
+    $mdDateLocaleProvider.months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
+        'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+    $mdDateLocaleProvider.shortMonths = ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun',
+        'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'];
+    $mdDateLocaleProvider.days = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sábado'];
+    $mdDateLocaleProvider.shortDays = ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sá'];
+    // Can change week display to start on Monday.
+    $mdDateLocaleProvider.firstDayOfWeek = 1;
+    $mdDateLocaleProvider.weekNumberFormatter = function (weekNumber) {
+        return 'Semana ' + weekNumber;
+    };
+    $mdDateLocaleProvider.msgCalendar = 'Calendario';
+    $mdDateLocaleProvider.msgOpenCalendar = 'Abrir calendario';
+    $mdDateLocaleProvider.formatDate = function (date) {
+        return moment(date).format('DD-MM-YYYY');
+    };
+});
+
+var moduleCommon = angular.module('commonControllers', []);
+var moduleService = angular.module('services', []);
+var moduleTipousuario = angular.module('tipousuarioControllers', []);
+var moduleUsuario = angular.module('usuarioControllers', []);
+var moduleProducto = angular.module('productoControllers', []);
+var moduleFactura = angular.module('facturaControllers', []);
+var moduleTipoproducto = angular.module('tipoproductoControllers', []);
+var moduleLogin = angular.module('loginControllers', []);
+var moduleCarrito = angular.module('carritoControllers', []);
+var moduleLinea = angular.module('lineaControllers', []);
+var moduleComponent = angular.module('moduleComponent', []);
+
+
+var moduloDirectivas = angular.module('Directives', []);
