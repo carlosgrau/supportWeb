3c0f2217aec9c950c06bf7ea1ebe40dcd1b9885f
a021792876p <a021792876p@s09-pc19.ausias.lan>
2019-02-14 20:51:11 +0100

    vamos mejoreando apariencia y completo crud cliente

--- src/main/webapp/js/app/cliente/view.js
+++ src/main/webapp/js/app/cliente/view.js
@@ -1,7 +1,7 @@
 'use strict';
 
-moduleCliente.controller('clienteViewController', ['$scope', '$http', 'toolService', '$routeParams', 'sessionService',
-    function ($scope, $http, toolService, $routeParams, sessionService) {
+moduleCliente.controller('clienteViewController', ['$scope', '$http', 'toolService', '$routeParams', 'sessionService', '$location',
+    function ($scope, $http, toolService, $routeParams, sessionService, $location) {
         $http({
             method: 'GET',
             withCredentials: true,
@@ -15,6 +15,47 @@ moduleCliente.controller('clienteViewController', ['$scope', '$http', 'toolServi
         });
 
         $scope.isActive = toolService.isActive;
+
+        $scope.atras = function () {
+            window.history.back();
+        };
+        $scope.editar = function (id) {
+            $location.path("/cliente/update/" + id);
+        };
+
+        $scope.borrar = function (id) {
+            swal({
+                title: "Estás seguro que quieres borrar este registro?",
+                text: "Una vez borres este registro no podrás recuperarlo.",
+                icon: "warning",
+                buttons: true,
+                dangerMode: true,
+            })
+                    .then((willDelete) => {
+                        if (willDelete) {
+                            $http({
+                                method: 'GET',
+                                withCredentials: true,
+                                url: 'json?ob=cliente&op=remove&ejercicio=' + sessionService.getEmpresa() + '&id=' + id
+                            }).then(function (response) {
+                                $scope.status = response.status;
+                                $scope.ajaxDataUsuarios = response.data.message;
+                                swal("El cliente ha sido borrado correctamente!", {
+                                    icon: "success",
+                                });
+                                 $location.path("/cliente/plist/");
+                            }, function (response) {
+                                $scope.ajaxDataUsuarios = response.data.message || 'Request failed';
+                                $scope.status = response.status;
+                            });
+
+                        } else {
+                            swal("Ha habido un error y no se ha podido borrar.");
+                        }
+                    });
+        }
+
+
 
 
     }]);
