ec4271830a0689b115fbd9d3e0f3a1444de0598a
Carlos <Carlos@192.168.1.102>
2019-05-12 04:10:48 +0200

    ESTOY A PUNTO DE QUE SE GUARDE SOLO FALTA LO QUE VIENE DEL JSON PASARLO AL BEAN DE PRESUPUESTO

--- src/main/java/com/service/CarritoService.java
+++ src/main/java/com/service/CarritoService.java
@@ -34,20 +34,20 @@ import javax.servlet.http.HttpSession;
  * @author Carlos
  */
 public class CarritoService {
-    
+
     Gson oGson = new Gson();
     ArrayList<ItemBean> carrito = null;
     HttpServletRequest oRequest;
     String ob = null;
     String usuario, password, conexion;
-    
+
     public CarritoService(HttpServletRequest oRequest) {
         super();
         this.oRequest = oRequest;
     }
-    
+
     public ReplyBean buyFactura() throws Exception {
-        
+
         ReplyBean oReplyBean = null;
         ConnectionInterface oConnectionPool = null;
         //Obtenemos la sesion actual
@@ -56,69 +56,69 @@ public class CarritoService {
         UsuarioBean oUsuarioBean = null;
         HikariConnectionForUser oHikariConectio = new HikariConnectionForUser();
         try {
-            
+
             Integer id = Integer.parseInt(oRequest.getParameter("id"));
             Integer empresa = Integer.parseInt(oRequest.getParameter("ejercicio"));
             oUsuarioBean = (UsuarioBean) oRequest.getSession().getAttribute("user");
-            
+
             usuario = oUsuarioBean.getLoginCli();
             password = oUsuarioBean.getPassCli();
             conexion = oUsuarioBean.newConnectionClient();
             oConnection = (Connection) oHikariConectio.newConnectionParams(usuario, password, conexion);
             oConnection.setAutoCommit(false);
             carrito = (ArrayList<ItemBean>) sesion.getAttribute("carrito");
-            
+
             FacturaBean oFacturaBean = new FacturaBean();
             FacturaDao oFacturaDao = new FacturaDao(oConnection, "dat140a");
             FacturaBean oFacturaBeanCreada = (FacturaBean) oFacturaDao.create(oFacturaBean);
             int id_factura = oFacturaBeanCreada.getFactura();
-            
+
             LineaFacturaDao oLineaFacturaDao;
             LineaFacturaBean oLineaFacturaBean;
-            
+
             ProductoDao oProductoDao = new ProductoDao(oConnection, "dat004a");
             oLineaFacturaDao = new LineaFacturaDao(oConnection, "dat141a");
             ProductoBean oProductoBean;
-            
+
             for (ItemBean ib : carrito) {
-                
+
                 float cant = ib.getCantidad();
-                
+
                 oLineaFacturaBean = new LineaFacturaBean();
-                
+
                 oLineaFacturaBean.setId_factura(id_factura);
                 oLineaFacturaBean.setEmpresa(empresa);
                 oLineaFacturaBean.setObj_Producto(ib.getObj_producto());
                 oLineaFacturaBean.setCantidad(cant);
-                
+
                 oLineaFacturaDao.create(oLineaFacturaBean);
-                
+
                 oProductoBean = new ProductoBean();
-                
+
                 oProductoBean.setId(ib.getObj_producto().getId());
-                
+
                 oProductoBean = ib.getObj_producto();
-                
+
                 oProductoBean.setExistencias((int) (oProductoBean.getExistencias() - cant));
-                
+
                 oProductoDao.update(oProductoBean);
-                
+
             }
-            
+
             oConnection.commit();
-            
+
             carrito.clear();
             sesion.setAttribute("carrito", carrito);
-            
+
             oReplyBean = new ReplyBean(200, oGson.toJson(id_factura));
         } catch (Exception e) {
-            
+
             try {
                 oConnection.rollback();
             } catch (SQLException excep) {
-                
+
             }
-            
+
             oReplyBean = new ReplyBean(500, "Error en buy carritoService: " + e.getMessage());
         } finally {
             if (oConnection != null) {
@@ -126,13 +126,13 @@ public class CarritoService {
             }
             oHikariConectio.disposeConnection();
         }
-        
+
         return oReplyBean;
-        
+
     }
 
     public ReplyBean buyPresupuesto() throws Exception {
-        
+
         ReplyBean oReplyBean = null;
         ConnectionInterface oConnectionPool = null;
         //Obtenemos la sesion actual
@@ -141,75 +141,81 @@ public class CarritoService {
         UsuarioBean oUsuarioBean = null;
         HikariConnectionForUser oHikariConectio = new HikariConnectionForUser();
         try {
-            
+
             Integer empresa = Integer.parseInt(oRequest.getParameter("ejercicio"));
             oUsuarioBean = (UsuarioBean) oRequest.getSession().getAttribute("user");
-            
+
             usuario = oUsuarioBean.getLoginCli();
             password = oUsuarioBean.getPassCli();
             conexion = oUsuarioBean.newConnectionClient();
             oConnection = (Connection) oHikariConectio.newConnectionParams(usuario, password, conexion);
             oConnection.setAutoCommit(false);
             carrito = (ArrayList<ItemBean>) sesion.getAttribute("carrito");
-            
+
             PresupuestoBean oPresupuestoBean = new PresupuestoBean();
             PresupuestoDao oPresupuestoDao = new PresupuestoDao(oConnection, "dat032a");
             String strJsonFromClient = oRequest.getParameter("json");
             Gson oGson = (new GsonBuilder()).excludeFieldsWithoutExposeAnnotation().create();
             oPresupuestoBean = oGson.fromJson(strJsonFromClient, PresupuestoBean.class);
-            
-            Integer presupuesto = oPresupuestoDao.getpresupuesto(empresa);            
+
+            //SACO EL ULTIMO NUMERO DEL PRESUPUESTO
+            Integer presupuesto = oPresupuestoDao.getpresupuesto(empresa);
             oPresupuestoBean.setPresupuesto(presupuesto + 1);
+            
+            //ANTES DE CREAR LA CABECERA DE PRESUPESTO TENEMOS QUE METER EN EL BEAN LOS DATOS DEL JSON
+            
+            
+            
             PresupuestoBean oPresupuestoBeanCreada = (PresupuestoBean) oPresupuestoDao.create(oPresupuestoBean);
             int id_presupuesto = oPresupuestoBeanCreada.getPresupuesto();
-            
+
             LineaPresupuestoDao oLineaPresupuestoDao;
             LineaPresupuestoBean oLineaPresupuestoBean;
-            
+
             ProductoDao oProductoDao = new ProductoDao(oConnection, "dat004a");
             oLineaPresupuestoDao = new LineaPresupuestoDao(oConnection, "dat131a");
             ProductoBean oProductoBean;
-            
+
             for (ItemBean ib : carrito) {
-                
+
                 float cant = ib.getCantidad();
-                
+
                 oLineaPresupuestoBean = new LineaPresupuestoBean();
-                
+
                 oLineaPresupuestoBean.setId_presupuesto(id_presupuesto);
                 oLineaPresupuestoBean.setId_dat032a(presupuesto + 1);
                 oLineaPresupuestoBean.setEmpresa(empresa);
                 oLineaPresupuestoBean.setObj_Producto(ib.getObj_producto());
                 oLineaPresupuestoBean.setCantidad(cant);
-                
+
                 oLineaPresupuestoDao.create(oLineaPresupuestoBean);
-                
+
                 oProductoBean = new ProductoBean();
-                
+
                 oProductoBean.setId(ib.getObj_producto().getId());
-                
+
                 oProductoBean = ib.getObj_producto();
-                
+
                 oProductoBean.setExistencias((int) (oProductoBean.getExistencias() - cant));
-                
+
                 oProductoDao.update(oProductoBean);
-                
+
             }
-            
+
             oConnection.commit();
-            
+
             carrito.clear();
             sesion.setAttribute("carrito", carrito);
-            
+
             oReplyBean = new ReplyBean(200, oGson.toJson(id_presupuesto));
         } catch (Exception e) {
-            
+
             try {
                 oConnection.rollback();
             } catch (SQLException excep) {
-                
+
             }
-            
+
             oReplyBean = new ReplyBean(500, "Error en buy carritoService: " + e.getMessage());
         } finally {
             if (oConnection != null) {
@@ -217,11 +223,11 @@ public class CarritoService {
             }
             oHikariConectio.disposeConnection();
         }
-        
+
         return oReplyBean;
-        
+
     }
-    
+
     public ReplyBean add() throws Exception {
         ReplyBean oReplyBean = null;
         ConnectionInterface oConnectionPool = null;
@@ -230,15 +236,15 @@ public class CarritoService {
         Connection oConnection = null;
         UsuarioBean oUsuarioBean = null;
         HikariConnectionForUser oHikariConectio = new HikariConnectionForUser();
-        
+
         try {
             Integer empresa = Integer.parseInt(oRequest.getParameter("ejercicio"));
             oUsuarioBean = (UsuarioBean) oRequest.getSession().getAttribute("user");
-            
+
             usuario = oUsuarioBean.getLoginCli();
             password = oUsuarioBean.getPassCli();
             conexion = oUsuarioBean.newConnectionClient();
-            
+
             oConnection = (Connection) oHikariConectio.newConnectionParams(usuario, password, conexion);
             //Si no existe la sesion creamos al carrito
             if (sesion.getAttribute("carrito") == null) {
@@ -252,7 +258,7 @@ public class CarritoService {
             float cantidadprod = Float.parseFloat(oRequest.getParameter("cantidad"));
             float descuentoprod = Float.parseFloat(oRequest.getParameter("descuento"));
             float precioprod = Float.parseFloat(oRequest.getParameter("precio"));
-            
+
             ProductoDao oProductoDao = new ProductoDao(oConnection, "dat004a");
             ProductoBean oProductoBean = (ProductoBean) oProductoDao.get(codigo, 1, empresa);
 
@@ -287,9 +293,9 @@ public class CarritoService {
             }
             //Actualizamos la sesion del carrito de compras
             sesion.setAttribute("carrito", carrito);
-            
+
             oReplyBean = new ReplyBean(200, oGson.toJson(carrito));
-            
+
         } catch (Exception ex) {
             // Logger.getLogger(CarritoService.class.getName()).log(Level.SEVERE, null, ex);
             oReplyBean = new ReplyBean(500, "Error en add carrito: " + ex.getMessage());
@@ -300,61 +306,61 @@ public class CarritoService {
             oHikariConectio.disposeConnection();
         }
         return oReplyBean;
-        
+
     }
-    
+
     public ReplyBean show() throws Exception {
         ReplyBean oReplyBean;
-        
+
         HttpSession sesion = oRequest.getSession();
-        
+
         if (sesion.getAttribute("carrito") == null) {
             oReplyBean = new ReplyBean(200, EncodingHelper.quotate("Carrito vacio"));
         } else {
             oReplyBean = new ReplyBean(200, oGson.toJson(sesion.getAttribute("carrito")));
         }
-        
+
         return oReplyBean;
-        
+
     }
-    
+
     public ReplyBean empty() {
         ReplyBean oReplyBean;
-        
+
         HttpSession sesion = oRequest.getSession();
-        
+
         if (sesion.getAttribute("carrito") == null) {
             oReplyBean = new ReplyBean(200, EncodingHelper.quotate("El carrito ya esta vacio"));
         } else {
             sesion.setAttribute("carrito", null);
             oReplyBean = new ReplyBean(200, EncodingHelper.quotate("Carrito vacio"));
         }
-        
+
         return oReplyBean;
-        
+
     }
-    
+
     public ReplyBean reduce() throws Exception {
-        
+
         ReplyBean oReplyBean;
-        
+
         HttpSession sesion = oRequest.getSession();
-        
+
         if (sesion.getAttribute("carrito") == null) {
             oReplyBean = new ReplyBean(200, EncodingHelper.quotate("No hay carrito"));
         } else {
             carrito = (ArrayList<ItemBean>) sesion.getAttribute("carrito");
             Integer id = Integer.parseInt(oRequest.getParameter("prod"));
-            
+
             int indice = -1;
-            
+
             for (int i = 0; i < carrito.size(); i++) {
                 if (id == carrito.get(i).getObj_producto().getId()) {
                     indice = i;
                     break;
                 }
             }
-            
+
             if (indice == -1) {
                 oReplyBean = new ReplyBean(200, EncodingHelper.quotate("El producto no esta en el carrito"));
             } else {
@@ -366,20 +372,20 @@ public class CarritoService {
                 } else {
                     carrito.remove(indice);
                 }
-                
+
                 if (carrito.size() < 1) {
                     sesion.setAttribute("carrito", null);
                     oReplyBean = new ReplyBean(200, EncodingHelper.quotate("Carrito vacio"));
-                    
+
                 } else {
                     sesion.setAttribute("carrito", carrito);
                     oReplyBean = new ReplyBean(200, oGson.toJson(carrito));
                 }
-                
+
             }
         }
         return oReplyBean;
-        
+
     }
-    
+
 }
